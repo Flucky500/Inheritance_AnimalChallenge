ffaefb9aa1a142f383d242539c2c31a6646f8cd7
Lukasz Pasko <flucky500@gmail.com>
2022-03-16 23:02:12 +0100

    Initial commit

---
+++ src/main/java/pl/pasko/Animal.java
@@ -0,0 +1,10 @@
+package pl.pasko;
+
+public class Animal {
+
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/pl/pasko/Main.java
@@ -0,0 +1,12 @@
+package pl.pasko;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Jelly", "Shi Tzu");
+        System.out.println(dog.getBreed());
+
+    }
+
+}
